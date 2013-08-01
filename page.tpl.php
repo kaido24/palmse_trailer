d7bdf36f2b8b138978440cf2ea0a5e7f020eba1f
Kaido Toomingas <root@web3.(none)>
2013-08-01 03:49:37 +0000

    add changes to git

--- page.tpl.php
+++ page.tpl.php
@@ -26,7 +26,6 @@
     <div class="clearfix container">
       <?php print $messages; ?>
       <a id="main-content"></a>
-      <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
       <?php print render($title_suffix); ?>
